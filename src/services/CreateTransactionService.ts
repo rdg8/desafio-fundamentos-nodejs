dce68f05d84615867817c0d8ef7054a87af4a863
rodrigo <digorinaldi@gmail.com>
2020-05-07 01:16:14 -0300

    entrega

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,28 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Type is invalid');
+    }
+
+    if ('outcome'.includes(type)) {
+      const { total } = this.transactionsRepository.getBalance();
+
+      if (value > total) {
+        throw new Error(
+          'Outcome output value cannot be greater than the balance',
+        );
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
